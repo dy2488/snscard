70d38b51bbe8ac105aa8a06986128166d77e4b1f
yjpark0307 <yjpark0307@gmail.com>
2024-04-28 20:45:30 +0900

    깃허브 API기능 완성 + 주석 세부적으로 수정

--- src/main/java/API/TestAPI.java
+++ src/main/java/API/TestAPI.java
@@ -16,10 +16,10 @@ public class TestAPI
         System.out.println(imgUrl);
         */
 
-        //GITAPi test
+        //깃허브 최근 업데이트된 프로젝트 3개의 제목,업데이트일자 가져오기   //관련 클래스 : GithubAPI
         GithubAPI g = new GithubAPI();
-        List<Post> temp = g.getGithubApi("https://github.com/yjpark0307/"); //파라터 형식은 되도록 지켜주기 바람
-        System.out.println(temp);
+        List<Post> temp = g.getGithubApi("https://github.com/dy2488/"); //파라미터는 사용자정보에 저장된 깃허브 링크를 그대로 넘겨주면 됨
+        System.out.println(temp); //테스트 출력 알고리즘
 
 
 
--- src/main/java/API/api/GithubAPI.java
+++ src/main/java/API/api/GithubAPI.java
@@ -10,7 +10,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Collections;
 
 public class GithubAPI
 {
@@ -18,36 +20,41 @@ public class GithubAPI
     public GithubAPI()
     {
     }
-    public List<Post> getGithubApi(String sourceUrl) throws Exception //주요 메서드
+    public List<Post> getGithubApi(String sourceUrl) throws Exception //호출되는 메서드
     {
-        UrlParser u = new UrlParser(); //받은 URL 파싱
-        String userName = u.parseUrl(sourceUrl,1);
-        String requestUrl = Github_Url + userName +"/repos";
-        String response = requestHttp(requestUrl);
+        UrlParser u = new UrlParser();
+        String userName = u.parseUrl(sourceUrl,1); //받은 URL에서 사용자 정보 획득 (주어진 URL의 첫번째 영역 가져옴)
+        String requestUrl = Github_Url + userName +"/repos"; //요청 URL 제작
+        String response = requestHttp(requestUrl); //요청
         List<Post> result = parseGithubJson(response);
         return result;
     }
-    public List<Post> parseGithubJson(String jsonData) //받은 JSON 파싱 후 배열로 저장
+    private List<Post> parseGithubJson(String jsonData) //받은 JSON 파싱 후 배열로 저장하는 함수
     {
         JSONArray repos = new JSONArray(jsonData);
         List<Post> postList = new ArrayList<>();
-        String[][] result = new String[repos.length()][2];
         for(int i=0;i<repos.length();i++)
         {
-            JSONObject repo = repos.getJSONObject(i);
+            JSONObject repo = repos.getJSONObject(i); //가져온 정보를 저장
             String name = repo.getString("name");
             String updatedAt = repo.getString("updated_at");
 
-            int tIndex = updatedAt.indexOf('T');
+            int tIndex = updatedAt.indexOf('T'); //updatedAt을 int로 변환하는 과정
             String subString = updatedAt.substring(0, tIndex);
             String dateString = subString.replace("-","");
             int date = Integer.parseInt(dateString);
 
-            postList.add(new Post(name,date));
+            postList.add(new Post(name,date)); //객체배열 삽입
         }
-        return postList;
+
+        Collections.sort(postList, Comparator.comparingInt(Post::getDate).reversed()); //내림차순 정렬 후 최대 3개의 게시물 정보를 저장한 새로운 배열 리턴
+        int toIndex = Math.min(postList.size(), 3);
+        List<Post> firstThreePostList = postList.subList(0, toIndex);
+
+        return firstThreePostList;
     }
-    public String requestHttp(String requestUrl) throws IOException, InterruptedException { //http 요청
+    private String requestHttp(String requestUrl) throws IOException, InterruptedException //HTTP 요청 함수
+    {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(requestUrl))
@@ -55,6 +62,6 @@ public class GithubAPI
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return response.body(); //응답받은 JSON 리턴
         }
 }
