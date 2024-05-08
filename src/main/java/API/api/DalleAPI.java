9e2a6c428c0212f9e3af7d31b0f2e8014ee15f28
yjpark0307 <yjpark0307@gmail.com>
2024-05-08 18:01:56 +0900

    프롬프트 구성 알고리즘 수정

--- src/main/java/API/api/DalleAPI.java
+++ src/main/java/API/api/DalleAPI.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Random;
+
 import org.json.JSONObject;
 
 public class DalleAPI //이미지생성 AI 관련 클래스 입니다. 기본적으로 5개정도를 생성했었을때 0.1$의 비용이 듭니다. 현재 계정에는 10$가 충전되어있어 테스트 목적으로 넉넉히 사용 가능하나, 불필요하게 막쓰진 말아주세요.
@@ -13,16 +15,32 @@ public class DalleAPI //이미지생성 AI 관련 클래스 입니다. 기본적
     public String getDalleAPI(String q1,String q2,String q3,String q4, String q5, String apiKey) throws Exception //prompt를 입력하여 호출하면, 해당 프롬프트 대로 이미지를 생성후, 이미지 URL을 반환하는 함수
     {
         String prompt,imgUrl;
-        prompt = generatePrompt(q1,q2,q3,q4,q5);
+        int algorithmFlag=1;
+        prompt = generatePrompt(q1,q2,q3,q4,q5,algorithmFlag);
         imgUrl = requestHttp(prompt,apiKey);
         return imgUrl;
     }
-
-    private String generatePrompt(String q1,String q2,String q3,String q4, String q5)
+    private String generatePrompt(String q1,String impression,String style,String tone, String object, int algorithmFlag)
     {
-        String prompt = q1 + ", " + q2 + ", " + q3 + ", " + q4 + ", " + q5;
+        String prompt;
+        String texture = randomTexture();
+        switch(algorithmFlag)
+    {
+        case 1: prompt = "a " + style + " depicting " + texture + " textures that features " + tone + " using a " + q1 + " with emphasis on " + object + " emitting an aura of " + impression + "."; break;
+        default : prompt = q1 + ", " + impression + ", " + style + ", " + tone + ", " + object;
+
+    }
         return prompt;
     }
+
+    private String randomTexture()
+    {
+        String[] textureList = {"smooth stone","sandy beach", "polished wood", "soft velvet"};
+        Random random = new Random();
+        int index = random.nextInt(textureList.length);
+        return textureList[index];
+    }
+
     private String requestHttp(String prompt, String apiKey) throws Exception
     {
         HttpClient client = HttpClient.newHttpClient();
@@ -30,7 +48,7 @@ public class DalleAPI //이미지생성 AI 관련 클래스 입니다. 기본적
         data.put("prompt", prompt);
         data.put("n", 1);
         data.put("size", "1024x1024");
-        data.put("model", "dall-e-2");
+        data.put("model", "dall-e-3");
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(DalleUrl))
