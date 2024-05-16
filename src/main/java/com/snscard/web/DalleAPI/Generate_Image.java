2ee1f98884be3f54252b9f23d5a0982bda293916
dingyu <1439433776@qq.com>
2024-05-16 13:34:17 +0900

    사용자 정보 추가 및 수정기능완료, 이미지 생성 cut기능 limit  한 페이지 10장 보여는 효과완료

--- src/main/java/com/snscard/web/DalleAPI/Generate_Image.java
+++ src/main/java/com/snscard/web/DalleAPI/Generate_Image.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Generate_Image {
-    String apiKey="";
     public String getImage(String q1,String q2,String q3,String q4,String q5) throws Exception {
-       return new DalleAPI().getDalleAPI(q1,q2,q3,q4,q5,apiKey);
+       return new DalleAPI().getDalleAPI(q1,q2,q3,q4,q5,"");
     }
 }
